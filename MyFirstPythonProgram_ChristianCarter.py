ff817b57da6842a8b45dfc7c9021d882066f781e
CarterC16 <43424524+CarterC16@users.noreply.github.com>
2018-09-19 21:20:07 -0400

    Add files via upload
    First submission

---
+++ MyFirstPythonProgram_ChristianCarter.py
@@ -0,0 +1,13 @@
+#writing a simple one line code in python. From chapter 1 exercise 2 on page 28.
+#9/7/2018
+#CTI-110 Tutorial 1 My First Python Program
+#Christian Carter
+
+print('This is a test of IDLE.')
+
+
+print('This is a test of IDLE.'
+
+
+
+      )
