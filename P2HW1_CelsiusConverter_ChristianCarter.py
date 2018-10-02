adca1dd8c2d64a1290651a7142ecd6bc2125eacf
CarterC16 <43424524+CarterC16@users.noreply.github.com>
2018-10-02 12:29:52 -0400

    Add files via upload

---
+++ P2HW1_CelsiusConverter_ChristianCarter.py
@@ -0,0 +1,11 @@
+# create a program that converts celsius to fahrenheit
+# 9/18/2018
+# CTI-110 P2HW1 - Celsius Fahrenheit Converter
+# Christian Carter
+
+
+Celsius = float(input('Enter a temperature in Celsius: '))
+
+Fahrenheit = 9.0/5.0 * Celsius + 32
+
+print('The Fahrenheit is F', format(Fahrenheit, ',.1f'))
